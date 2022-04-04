65b06651b7a6dd423d4c380d17fc642ff36889a9
MENasri23 <m.e26159487@gmail.com>
2022-04-04 22:38:58 +0430

    Define new dagger module to inject database and its Daos

---
+++ database/src/main/java/com/example/database/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package com.example.database.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "todo-db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun provideTaskDao(database: AppDatabase) = database.taskDao()
+
+    @Provides
+    fun provideUserDao(database: AppDatabase) = database.userDao()
+}
