f8cc9415cf0deeba1b82babb20963dad2d295730
MENasri23 <m.e26159487@gmail.com>
2022-04-04 14:18:25 +0430

    Define new entity user

---
+++ shared/src/main/java/com/example/shared/data/User.kt
@@ -0,0 +1,23 @@
+package com.example.shared.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey // TODO: Define with auto generated property
+    val id: Long,
+
+    @ColumnInfo(name = "email")
+    val email: String,
+
+    @ColumnInfo(name = "username")
+    val username: String? = null,
+
+    @ColumnInfo(name = "first_name")
+    val firstName: String? = null,
+
+    @ColumnInfo(name = "last_name")
+    val lastName: String? = null,
+)
