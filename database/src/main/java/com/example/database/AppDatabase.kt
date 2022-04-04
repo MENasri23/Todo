bbb316c7ce2257ca09ad2c4246d21943807797f7
MENasri23 <m.e26159487@gmail.com>
2022-04-04 16:02:48 +0430

    Create new Database

---
+++ database/src/main/java/com/example/database/AppDatabase.kt
@@ -0,0 +1,21 @@
+package com.example.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.database.dao.TaskDao
+import com.example.database.dao.UserDao
+import com.example.shared.data.Task
+import com.example.shared.data.User
+
+@Database(
+    entities = [Task::class, User::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(DatabaseTypeConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+    abstract fun taskDao(): TaskDao
+}
