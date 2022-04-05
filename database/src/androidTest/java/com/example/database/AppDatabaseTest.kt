9318b570e909cc7fc4cae77f78aaf881c4da4c9c
MENasri23 <m.e26159487@gmail.com>
2022-04-05 13:19:28 +0430

    Create new class to test the room database

---
+++ database/src/androidTest/java/com/example/database/AppDatabaseTest.kt
@@ -0,0 +1,15 @@
+package com.example.database
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppDatabaseTest {
+
+
+}
