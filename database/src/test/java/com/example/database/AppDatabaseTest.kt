c836de0665767e43f39641f2f375544c40f19da7
MENasri23 <m.e26159487@gmail.com>
2022-04-10 21:14:16 +0430

    Define new property to run tests inside of coroutines

--- database/src/test/java/com/example/database/AppDatabaseTest.kt
+++ database/src/test/java/com/example/database/AppDatabaseTest.kt
@@ -6,17 +6,24 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.database.dao.TaskDao
 import com.example.database.dao.UserDao
 import com.example.shared.data.Task
+import com.example.test.shared.data.MainCoroutineRule
 import com.google.common.truth.Truth.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class AppDatabaseTest {
+
+    @get:Rule
+    var coroutineRule = MainCoroutineRule()
 
     private lateinit var database: AppDatabase
     private lateinit var userDao: UserDao
