7623f7dd176c7b418cb1edbe3ae40b2a5c76ff52
MENasri23 <m.e26159487@gmail.com>
2022-04-11 10:44:35 +0430

    Insert users and tasks with replacement conflict strategy

--- database/src/main/java/com/example/database/dao/TaskDao.kt
+++ database/src/main/java/com/example/database/dao/TaskDao.kt
@@ -2,6 +2,7 @@ package com.example.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.shared.data.Task
 import kotlinx.coroutines.flow.Flow
@@ -9,10 +10,10 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TaskDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: Task)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTasks(tasks: List<Task>)
 
     @Query("SELECT * FROM tasks AS t WHERE t.owner_id = :userId")
--- database/src/main/java/com/example/database/dao/UserDao.kt
+++ database/src/main/java/com/example/database/dao/UserDao.kt
@@ -2,13 +2,14 @@ package com.example.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.shared.data.User
 
 @Dao
 interface UserDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUser(user: User): Long
 
     @Query("SELECT * FROM users WHERE id = :userId LIMIT 1")
