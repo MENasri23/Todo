8fbb2e25dc19ddf90ed074117a61ed439c26b70f
MENasri23 <m.e26159487@gmail.com>
2022-04-18 17:34:35 +0430

    Remove additional buildScript block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
-buildscript {
-    val agp_version by extra("7.2.0")
-    dependencies {
-        classpath("com.android.tools.build:gradle")
-    }
-}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application").version("7.1.2").apply(false)
