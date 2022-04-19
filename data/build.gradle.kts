35d6103987f6c064297720f67e8bbcbe49e7f1bc
MENasri23 <m.e26159487@gmail.com>
2022-04-19 15:15:07 +0430

    Add database module to data layer

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -35,8 +35,12 @@ android {
 }
 
 dependencies {
+    implementation(project(":model"))
+    implementation(project(":database"))
 
     implementAppLibraries()
+
+    implementation(Libs.Kotlinx.COROUTINES_ANDROID)
     /*testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")*/
