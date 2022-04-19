e4af0aca4631279d3e5f7c3749506d71f85a15e6
MENasri23 <m.e26159487@gmail.com>
2022-04-19 15:17:19 +0430

    Create an abstract local data source associated with tasks

---
+++ data/src/main/java/com/example/data/source/local/TaskLocalDataSource.kt
@@ -0,0 +1,17 @@
+package com.example.data.source.local
+
+import com.example.database.view.TaskDetail
+import com.example.model.entity.Task
+import kotlinx.coroutines.flow.Flow
+
+interface TaskLocalDataSource {
+
+    suspend fun insertTasks(tasks: List<Task>): Boolean
+
+    fun getUserTaskDetails(userId: Long): Flow<List<TaskDetail>>
+
+    fun findTaskDetailById(id: String): Flow<TaskDetail>
+
+    suspend fun removeTasks(tasks: List<Task>): Boolean
+
+}
