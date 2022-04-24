93d19323b6d4593ae6369e5d3616ce8eec4a8a39
MENasri23 <m.e26159487@gmail.com>
2022-04-25 01:13:59 +0430

    Create new module for injecting dispatchers with qualifiers

---
+++ data/src/main/java/com/example/data/di/DispatcherModule.kt
@@ -0,0 +1,38 @@
+package com.example.data.di
+
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+
+@Module
+class DispatcherModule {
+
+    @IODispatcher
+    @Provides
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @CpuDispatcher
+    @Provides
+    fun provideCpuDispatcher(): CoroutineDispatcher = Dispatchers.Default
+}
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IODispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class CpuDispatcher
+
