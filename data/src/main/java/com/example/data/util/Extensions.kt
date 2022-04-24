c0458ea94ac2ec81b7eb18fb2fd4d2d15c04fb43
MENasri23 <m.e26159487@gmail.com>
2022-04-24 12:38:30 +0430

    Change declaration of toResource function and fix it

--- data/src/main/java/com/example/data/util/Extensions.kt
+++ data/src/main/java/com/example/data/util/Extensions.kt
@@ -3,12 +3,14 @@ package com.example.data.util
 import Resource
 import kotlinx.coroutines.flow.*
 
-fun <T> Flow<T>.toResource(): Flow<Resource<T>> = flow {
-    map {
-        Resource.success(it)
+fun <T> Flow<T>.toResource(): Flow<Resource<T>> =
+    map { result ->
+        Resource.success(result)
     }.onStart {
         emit(Resource.loading())
     }.catch { cause ->
         emit(Resource.error(cause))
     }
-}
+
+
+
