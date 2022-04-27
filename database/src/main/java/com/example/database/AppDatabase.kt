8f4e4738e47cca60e189655ae8ea35dcc1ff781c
MENasri23 <m.e26159487@gmail.com>
2022-04-27 22:54:18 +0430

    Add category dao to room database class

--- database/src/main/java/com/example/database/AppDatabase.kt
+++ database/src/main/java/com/example/database/AppDatabase.kt
@@ -3,14 +3,20 @@ package com.example.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.database.dao.CategoryDao
 import com.example.database.dao.TaskDao
 import com.example.database.dao.UserDao
 import com.example.database.view.TaskDetail
+import com.example.model.entity.Category
 import com.example.model.entity.Task
 import com.example.model.entity.User
 
 @Database(
-    entities = [Task::class, User::class],
+    entities = [
+        Task::class,
+        User::class,
+        Category::class
+    ],
     views = [TaskDetail::class],
     version = 1,
     exportSchema = false
@@ -20,4 +26,5 @@ abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
     abstract fun taskDao(): TaskDao
+    abstract fun categoryDao(): CategoryDao
 }
