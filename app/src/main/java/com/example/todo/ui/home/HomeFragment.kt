bc4908b977e04befaab7dd88828a24b2a5893211
MENasri23 <m.e26159487@gmail.com>
2022-05-01 06:59:51 -0700

    Create new page for home screen

---
+++ app/src/main/java/com/example/todo/ui/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.todo.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todo.R
+import com.example.todo.databinding.FragmentHomeBinding
+import com.example.todo.util.viewBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
